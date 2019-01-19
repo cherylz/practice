fc1fa4289e38bdc20379a6da7272c3963c8c06a1
cz <deutschessz@gmail.com>
2019-01-19 09:46:08 +0800

    remove files

--- catch-of-the-day/src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-  <div>
-    <h2>Wow... Not Found!?!!</h2>
-  </div>
-);
-
-export default NotFound;
--- catch-of-the-day/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Router from './components/Router';
-import './css/style.css';
-// why don't we need to import css in the component files?
-
-render(<Router />, document.querySelector('#main'));
